5007e2cc7fe510f2780512cf29b83a79eafa3eff
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-05 13:43:37 -0600

    navegación y página de favoritos

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,7 @@
-import { Spacer, Text, useTheme } from "@nextui-org/react";
+import { Spacer, Text, useTheme, Link } from "@nextui-org/react";
 import Image from "next/image";
+// Estamos forzados a importar el Link con otro nombre dado que usamos una librería de UI que tiene un componente con el mismo nombre
+import NextLink from "next/link";
 
 export const Navbar = () => {
   // Usar el tema cargado en NextUI
@@ -27,14 +29,24 @@ export const Navbar = () => {
         width={70}
         height={70}
       />
-      <Text color="white" h2>
-        P
-      </Text>
-      <Text color="white" h3>
-        okemon
-      </Text>
+
+      {/* No hay necesidad de hacer toda esta vaina
+      Con CSS podemos dejar el contenido del enlace en linea
+      Pero NextUI nos ayuda con eso */}
+      <Link href="/" as={NextLink}>
+        <Text color="white" h2>
+          P
+        </Text>
+        <Text color="white" h3>
+          okemon
+        </Text>
+      </Link>
+
       <Spacer css={{ flex: 1 }} />
-      <Text color="white">Favoritos</Text>
+
+      <NextLink href="/favorites">
+        <Text color="white">Favoritos</Text>
+      </NextLink>
     </div>
   );
 };
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+export const FavoritesPage: NextPage = () => {
+  return (
+    <div>FavoritesPage</div>
+  )
+}
+
+export default FavoritesPage;
