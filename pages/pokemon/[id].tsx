0e96a560a4b96bd77bf6baeb8314a060c9d13cc7
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-05 12:34:18 -0600

    generar las 151 páginas de pokemon

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,19 +1,65 @@
 import { Layout } from '@/components/layouts'
 import { useRouter } from 'next/router'
 import React, { FC } from 'react'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import { pokemonApi } from '@/api'
+import { Pokemon } from '@/interfaces'
 
-const PokemonPage: FC = () => {
+interface Props {
+    pokemon: Pokemon
+}
 
-    const router = useRouter();
-    
-    // Recuperar el parametro de consulta enviado a esta ruta
-    const id = router.query.id;
+const PokemonPage: NextPage<Props> = ({pokemon}) => {
 
   return (
     <Layout title="Algún pokemon">
-        <h3>Información del Pokemon {id}</h3>
+        <h3>Información del Pokemon {pokemon.name}</h3>
     </Layout>
   )
+}
+
+
+/**
+ * getStaticPaths
+ * 
+ * Se debe usar cuando estamos pre-redenderizando páginas estáticas que usan rutas dinámicas
+ * Esto sucede en tiempo de construcción (del lado del servidor)
+ * 
+ * NextJS se tiene que anticipar a las futuras consultas para saber que información mostrar en esta página
+ * 
+ */
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+    // Generar el listado de paths con los parámetros de busqueda admitidos por la aplicacón
+    // Se tienen 151 pokemon, no habrá más
+
+    // Generar un arreglo de 151 posiciones, cuyos elementos van del 1 al 151 (string)
+    const pokemonList = [...Array(151)].map((value, index) => `${index + 1}`);
+  
+    return {
+        paths: pokemonList.map(id => ({
+            params: {id}
+        })),
+        fallback: false     // Si el id no aparece en los registros, mostrar 404
+    }
+}
+
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    // Recuperar del conexto los parámetros admitidos por la aplicación para mostrar esta página
+    const { id } = ctx.params as {id: string}
+
+    // Hacer la petición HTTP en tiempo de construcción
+    // Para cachear los resultados y servirlos de forma estática en futuras peticiones
+    const {data} = await pokemonApi.get<Pokemon>(`/pokemon/${id}`);
+
+    // Pasar los resultados como Props a la página de Next
+    return {
+        props: {
+            pokemon: data
+        }
+    }
 }
 
 export default PokemonPage;
